57bd5cc8e492ca8b42fe257fb61e014e0bee90a8
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-02-06 00:24:56 -0300

    almost get it

--- src/index.js
+++ src/index.js
@@ -15,10 +15,76 @@ function preprocess_yaml(yaml_string) {
     .filter((line) => !line.startsWith("#") && line.trim() !== "");
 }
 
-function parseYaml(yamlArr) {}
+function parseYaml(yamlArray) {
+  const positionToParent = new Array(yamlArray.length).fill(0);
+
+  for (let i = 0; i < yamlArray.length; i++) {
+    const line = yamlArray[i];
+    const indent = getBeginningSpaces(line);
+
+    if (indent === 0) {
+      positionToParent[i] = 0;
+      continue;
+    }
+    getParent(yamlArray, i, positionToParent);
+  }
+
+  return positionToParent;
+}
+
+function getParent(arr, i, positionToParent) {
+  let j = i - 1;
+  while (j >= 0) {
+    if (getBeginningSpaces(arr[j]) < getBeginningSpaces(arr[i])) {
+      positionToParent[i] = j;
+      break;
+    }
+    j--;
+  }
+}
 
 function getBeginningSpaces(line) {
   return line.search(/\S|$/);
+}
+
+function constructObject(lines, parents) {
+  const result = {};
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const parentIndex = parents[i];
+
+    const currentObject = {};
+
+    if (parentIndex === 0) {
+      const keyValue = line.split(":");
+      result[keyValue[0]] =
+        keyValue[1] === "" ? currentObject : keyValue[1].trim();
+    } else {
+      const parent = lines[parentIndex].split(":");
+      const keyValue = line.split(":");
+      let value = keyValue === "" ? "" : keyValue[1];
+      agregarKeyValue(result, keyValue[0].trim(), parent[0].trim(), value);
+    }
+  }
+
+  return result;
+}
+
+function agregarKeyValue(objeto, key, target, value) {
+  if (objeto.hasOwnProperty(target)) {
+    objeto[target][key] = value || {};
+    return true;
+  }
+
+  for (const prop in objeto) {
+    if (typeof objeto[prop] === "object") {
+      if (agregarKeyValue(objeto[prop], key, target, value)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 function main() {
@@ -27,8 +93,9 @@ function main() {
     const content = readFileSync(filepath, "utf8");
     const yaml_string = content.toString();
     const yaml_preprocessed = preprocess_yaml(yaml_string);
-    const parsedYaml = parseYaml(yaml_preprocessed);
-    console.log(parsedYaml);
+    const positionToParent = parseYaml(yaml_preprocessed);
+    const json = constructObject(yaml_preprocessed, positionToParent);
+    console.log(json);
   } catch (error) {
     console.error("Something went wrong while reading the file", error);
   }
