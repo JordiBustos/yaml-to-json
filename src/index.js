35ba31f4942f3520633a527da9ddfed24381911a
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-02-06 00:47:43 -0300

    make it work with unique keys.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { readFileSync } from "fs";
-import { get } from "http";
 
 /**
  * Preprocess the yaml string to remove any comments and whitelines
@@ -16,11 +15,16 @@ function preprocess_yaml(yaml_string) {
 }
 
 function parseYaml(yamlArray) {
+  const maxIndent =
+    new Set([...yamlArray.map((x) => getBeginningSpaces(x))]).size - 1;
+  const indexToIndent = new Array(maxIndent + 1).fill().map(() => []);
   const positionToParent = new Array(yamlArray.length).fill(0);
 
   for (let i = 0; i < yamlArray.length; i++) {
     const line = yamlArray[i];
     const indent = getBeginningSpaces(line);
+
+    indexToIndent[indent / 2].push(i);
 
     if (indent === 0) {
       positionToParent[i] = 0;
@@ -29,7 +33,7 @@ function parseYaml(yamlArray) {
     getParent(yamlArray, i, positionToParent);
   }
 
-  return positionToParent;
+  return [indexToIndent, positionToParent];
 }
 
 function getParent(arr, i, positionToParent) {
@@ -47,8 +51,9 @@ function getBeginningSpaces(line) {
   return line.search(/\S|$/);
 }
 
-function constructObject(lines, parents) {
+function constructObject(lines, parents, indexToIndent) {
   const result = {};
+  console.log(indexToIndent);
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
     const parentIndex = parents[i];
@@ -60,10 +65,11 @@ function constructObject(lines, parents) {
       result[keyValue[0]] =
         keyValue[1] === "" ? currentObject : keyValue[1].trim();
     } else {
-      const parent = lines[parentIndex].split(":");
+      const parent = lines[parentIndex].split(":")[0].trim();
       const keyValue = line.split(":");
       let value = keyValue === "" ? "" : keyValue[1];
-      agregarKeyValue(result, keyValue[0].trim(), parent[0].trim(), value);
+
+      agregarKeyValue(result, keyValue[0].trim(), parent, value);
     }
   }
 
@@ -93,8 +99,12 @@ function main() {
     const content = readFileSync(filepath, "utf8");
     const yaml_string = content.toString();
     const yaml_preprocessed = preprocess_yaml(yaml_string);
-    const positionToParent = parseYaml(yaml_preprocessed);
-    const json = constructObject(yaml_preprocessed, positionToParent);
+    const [indexToIndent, positionToParent] = parseYaml(yaml_preprocessed);
+    const json = constructObject(
+      yaml_preprocessed,
+      positionToParent,
+      indexToIndent
+    );
     console.log(json);
   } catch (error) {
     console.error("Something went wrong while reading the file", error);
