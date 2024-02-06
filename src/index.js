e7de43d9a1e8980b176e8fdf533b85d2bd40e76c
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-02-05 22:46:24 -0300

    remove not working function

--- src/index.js
+++ src/index.js
@@ -15,55 +15,10 @@ function preprocess_yaml(yaml_string) {
     .filter((line) => !line.startsWith("#") && line.trim() !== "");
 }
 
-function getIndentationReferences(yamlArray) {
-  const references = {};
-  let currentParent = yamlArray[0];
+function parseYaml(yamlArr) {}
 
-  for (let i = 0; i < yamlArray.length; i++) {
-    if (
-      !yamlArray[i].includes("-") &&
-      !yamlArray[i].endsWith(":") &&
-      countWhitespaces(yamlArray[i].split(":")[0]) === 0
-    ) {
-      const splitted = yamlArray[i].split(":");
-      references[splitted[0]] = splitted[1].trim();
-      continue;
-    }
-
-    const line = yamlArray[i];
-    const indentation = countWhitespaces(line);
-    const [key, value] = getKeyValueFromLine(line);
-    if (indentation === 0) {
-      currentParent = key;
-      references[currentParent] = {};
-    } else {
-      references[currentParent][key] = value;
-    }
-  }
-
-  return references;
-}
-
-/**
- * Get the key value pair from the line
- * @param {string} line - The line to get the key value pair
- * @return {string[]} - The key value pair
- */
-function getKeyValueFromLine(line) {
-  const [key, value] = line.split(":");
-  return [key.trim(), value];
-}
-
-/**
- * Count the number of whitespaces in the line
- * @param {string} line - The line to count the whitespaces
- * @return {number} - The number of whitespaces
- */
-function countWhitespaces(line) {
-  return line
-    .split(":")[0]
-    .split("")
-    .filter((char) => char === " ").length;
+function getBeginningSpaces(line) {
+  return line.search(/\S|$/);
 }
 
 function main() {
@@ -72,7 +27,8 @@ function main() {
     const content = readFileSync(filepath, "utf8");
     const yaml_string = content.toString();
     const yaml_preprocessed = preprocess_yaml(yaml_string);
-    console.log(getIndentationReferences(yaml_preprocessed));
+    const parsedYaml = parseYaml(yaml_preprocessed);
+    console.log(parsedYaml);
   } catch (error) {
     console.error("Something went wrong while reading the file", error);
   }
